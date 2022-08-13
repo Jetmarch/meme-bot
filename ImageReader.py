12ce58f2a29c1761f32e97a34611d92341c519c2
Andrew <j3tmarch@gmail.com>
2022-08-13 22:10:56 +0300

    Добавлена обёртка взаимодействия с ВК. Эксперименты с ImageReader

--- ImageReader.py
+++ ImageReader.py
@@ -1,17 +1,24 @@
 import string
+import cv2
+import numpy as np
+import easyocr
+import matplotlib.pyplot as plt
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
+
 class ImageReader:
+    used_lang = 'eng+rus'
     def __init__(self) -> None:
         pytesseract.pytesseract.tesseract_cmd = 'D:\\Tesseract\\tesseract.exe'
 
     def read(self, path_to_image, lang='rus') -> string:
-        im = Image.open(path_to_image)
-        im = im.filter(ImageFilter.MedianFilter())
-        enhancer = ImageEnhance.Contrast(im)
-        im = enhancer.enhance(2)
-        im = im.convert('1')
-        im.save('temp2.jpg')
-        text = pytesseract.image_to_string(Image.open('temp2.jpg'), lang=lang)
+        #image = cv2.imread(path_to_image, 0)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #text = pytesseract.image_to_string(image, lang=self.used_lang)
+        text = self.recognize_text(path_to_image)
         return text
+
+    def recognize_text(self, img_path):
+        reader = easyocr.Reader(['en', 'ru'])
+        return reader.readtext(img_path)
--- VKBot.py
+++ VKBot.py
@@ -1,39 +1,69 @@
 import time
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from ImageLoader import ImageLoader
+from ImageReader import ImageReader
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import detectlanguage
 
+class VkBot:
+    token = ''
+    english_conf_id = 0
+    russian_conf_id = 0
+    bot_admin_id = 365491689
 
-def write_msg(chat_id, message):
-    random_id = int(round(time.time() * 1000))
-    vk.method('messages.send', {'chat_id': chat_id, 'message': message, 'random_id':random_id})
+    def __init__(self) -> None:
+        f = open('token.txt', 'r', encoding="utf-8")
+        self.token = f.readline()
+        f.close()
+        f = open('lang_detect_key.txt', 'r', encoding="utf-8")
+        detectlanguage.configuration.api_key = f.readline()
+        f.close()
+        self.vk = vk_api.VkApi(token=self.token)
+        self.longpoll = VkBotLongPoll(self.vk, '215306309')
+        self.imageReader = ImageReader()
+        print('Bot launched')
 
+    def write_msg(self, chat_id, message):
+        random_id = int(round(time.time() * 1000))
+        self.vk.method('messages.send', {'chat_id': chat_id, 'message': message, 'random_id':random_id})
+        print(message)
 
-f = open('token.txt', 'r', encoding="utf-8")
-token = f.readline()
+    def listen_longpoll(self):
+        for event in self.longpoll.listen():
+            try:
+                print('event from: ' + str(event.chat_id))
+                if event.type == VkBotEventType.MESSAGE_NEW:
+                    if event.from_chat:
 
-f.close()
+                        if event.message.text == '!rus' and event.message.from_id == self.bot_admin_id:
+                            self.english_conf_id = event.chat_id
+                            self.write_msg(event.chat_id, 'Запомнил')
+                        if event.message.text == '!eng' and event.message.from_id == self.bot_admin_id:
+                            self.russian_conf_id = event.chat_id
+                            self.write_msg(event.chat_id, 'Запомнил')
 
-vk = vk_api.VkApi(token=token)
-longpoll = VkBotLongPoll(vk, '215306309')
+                        #print(str(event))
+                        #Загрузка самой большой картинки из вложения в сообщении
+                        if len(event.message.attachments) != 0:
+                            for size in event.message.attachments[0]['wall']['attachments'][0]['photo']['sizes']:
+                                if(size['type'] == 'r'):
+                                    ImageLoader.load_from_url(size['url'])
+                            text = self.imageReader.read('temp.jpg')
+                            print(text)
+                            try:
+                                lang = detectlanguage.simple_detect(text)
+                            
+                                print(lang)
+                                self.write_msg(event.chat_id, 'Что я прочитал: ' + text + '\n' 
+                                    + ' Какой язык я определил: ' + lang)
+                            except Exception as e:
+                                self.write_msg(event.chat_id, 'Не нашёл на пикче слов')
+                                print(str(e))
+            except Exception as e:
+                self.write_msg(event.chat_id, 'Сломалься.')
+                print(str(e))
 
-print('Launched')
+bot = VkBot()
 
-'''
-TODO: Добавить команду, дающую возможность запомнить айди беседы 
-        для дальнейшего распределения сообщений
-      Пока что планируется распределение только по языку картинки (английский и русский)
-
-'''
-for event in longpoll.listen():
-    print('event from: ' + str(event.chat_id))
-    
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        if event.from_chat:
-            print('event from chat: ' + event.message.text)
-            
-            if event.message.text == 'Boi?':
-                write_msg(event.chat_id, 'Oh yeah. Im fkin alive')
-            else:
-                write_msg(event.chat_id, 'I sleep.')
+bot.listen_longpoll()
